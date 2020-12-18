871f3ddf0f08691829068d4508934e65ba626a7c
betula <mail@betula.co>
2020-12-18 14:00:52 +0700

    chore: dependencies updated

--- .babelrc.js
+++ .babelrc.js
@@ -5,6 +5,6 @@ module.exports = {
   "plugins": [
     ["@babel/plugin-proposal-decorators", { "legacy": true }],
     ["@babel/plugin-proposal-class-properties", { "loose": true }],
-    "realar/babel"
+    "realar"
   ]
 }
